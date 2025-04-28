4b9bc475a8531613b8b3a070e46785b0efcfa7e0
Ashok Bk <ashokbk215@gmail.com>
2025-04-28 20:09:31 +0545

    fix deploy

--- src/helpers.py
+++ src/helpers.py
@@ -1,21 +1,5 @@
-from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader #for loading pdf files
-from langchain.text_splitter import RecursiveCharacterTextSplitter  #for chunking
 from sentence_transformers import SentenceTransformer
-
-# Extract text from PDF files
-def load_pdf_file(data):
-    loader = DirectoryLoader(data, glob="**/*.pdf", loader_cls=PyPDFLoader)
-    documents = loader.load()
-    return documents
-
-# Split the data into chunks
-def text_split(extracted_data):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=20,
-    )
-    text_chunks = text_splitter.split_documents(extracted_data)
-    return text_chunks
+import os
 
 # Class for Hugging Face embeddings
 class HuggingFaceEmbeddings:
